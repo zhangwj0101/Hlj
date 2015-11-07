94b93c3d600a5f51393b9dd5b5efe30a834c3d66
zhangwj0101 <zhangwj0101@gmail.com>
2015-11-07 11:06:52 +0800

    添加科技产业

--- WebRoot/hlj_V2/kejichanye/js/functions.js
+++ WebRoot/hlj_V2/kejichanye/js/functions.js
@@ -17,6 +17,79 @@ jQuery(function() {
 				url         : "#", // url where image will link
 				alt         : "Speed Optimized" // image alt tag
 			},
+			{
+				display     : "image", // image, map, video
+				title       : "Nex", // image title
+				description : "Speed Optimized", // image description
+				link        : "images/2.jpg", // image src
+				thumb       : "images/2.jpg", // image thumb
+				url         : "#", // url where image will link
+				alt         : "Speed Optimized" // image alt tag
+			},
+			{
+				display     : "image", // image, map, video
+				title       : "Nex", // image title
+				description : "Speed Optimized", // image description
+				link        : "images/3.jpg", // image src
+				thumb       : "images/3.jpg", // image thumb
+				url         : "#", // url where image will link
+				alt         : "Speed Optimized" // image alt tag
+			},
+			{
+				display     : "image", // image, map, video
+				title       : "Nex", // image title
+				description : "Speed Optimized", // image description
+				link        : "images/4.jpg", // image src
+				thumb       : "images/4.jpg", // image thumb
+				url         : "#", // url where image will link
+				alt         : "Speed Optimized" // image alt tag
+			},
+			{
+				display     : "image", // image, map, video
+				title       : "Nex", // image title
+				description : "Speed Optimized", // image description
+				link        : "images/5.jpg", // image src
+				thumb       : "images/5.jpg", // image thumb
+				url         : "#", // url where image will link
+				alt         : "Speed Optimized" // image alt tag
+			},
+			{
+				display     : "image", // image, map, video
+				title       : "Nex", // image title
+				description : "Speed Optimized", // image description
+				link        : "images/6.jpg", // image src
+				thumb       : "images/6.jpg", // image thumb
+				url         : "#", // url where image will link
+				alt         : "Speed Optimized" // image alt tag
+			},
+			{
+				display     : "image", // image, map, video
+				title       : "Nex", // image title
+				description : "Speed Optimized", // image description
+				link        : "images/7.jpg", // image src
+				thumb       : "images/7.jpg", // image thumb
+				url         : "#", // url where image will link
+				alt         : "Speed Optimized" // image alt tag
+			},
+			{
+				display     : "image", // image, map, video
+				title       : "Nex", // image title
+				description : "Speed Optimized", // image description
+				link        : "images/8.jpg", // image src
+				thumb       : "images/8.jpg", // image thumb
+				url         : "#", // url where image will link
+				alt         : "Speed Optimized" // image alt tag
+			},
+			{
+				display     : "image", // image, map, video
+				title       : "Nex", // image title
+				description : "Speed Optimized", // image description
+				link        : "images/9.jpg", // image src
+				thumb       : "images/9.jpg", // image thumb
+				url         : "#", // url where image will link
+				alt         : "Speed Optimized" // image alt tag
+			},
+			
 		]
 	});
 
--- WebRoot/hlj_V2/kejizhanl/js/functions.js
+++ WebRoot/hlj_V2/kejizhanl/js/functions.js
@@ -8,15 +8,7 @@ jQuery(function() {
 		load_bar         : true, // true || false
 
 		data             : [
-			{
-				display     : "image", // image, map, video
-				title       : "Nex", // image title
-				description : "Blazing Fast Fullscreen Slider", // image description
-				link        : "images/17.jpg", // image src
-				thumb       : "images/17.jpg", // image thumb
-				url         : "#", // url where image will link
-				alt         : "Blazing Fast Fullscreen Slider" // image alt tag
-			},
+
 			{
 				display     : "image", // image, map, video
 				title       : "Nex", // image title
@@ -26,6 +18,26 @@ jQuery(function() {
 				url         : "#", // url where image will link
 				alt         : "Speed Optimized" // image alt tag
 			},
+			{
+				display     : "image", // image, map, video
+				title       : "Nex", // image title
+				description : "Speed Optimized", // image description
+				link        : "images/3.jpg", // image src
+				thumb       : "images/3.jpg", // image thumb
+				url         : "#", // url where image will link
+				alt         : "Speed Optimized" // image alt tag
+			},
+			
+			{
+				display     : "image", // image, map, video
+				title       : "Nex", // image title
+				description : "Blazing Fast Fullscreen Slider", // image description
+				link        : "images/17.jpg", // image src
+				thumb       : "images/17.jpg", // image thumb
+				url         : "#", // url where image will link
+				alt         : "Blazing Fast Fullscreen Slider" // image alt tag
+			},
+
 		]
 	});
 
