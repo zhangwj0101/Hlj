0eacc4a8c40ba689f5ca9754c3b0fa8457a97094
zhangwj0101 <zhangwj0101@gmail.com>
2015-11-13 11:00:50 +0800

    添加79家事业单位

---
+++ WebRoot/hlj_V2/qiye/js/functions.js
@@ -0,0 +1,96 @@
+jQuery(function() {
+
+	new Nex({
+
+		thumbnails       : true, // true || false
+		bullets          : true,  // true || false
+		bar              : false, // true || false
+		load_bar         : true, // true || false
+
+		data             : [
+			{
+				display     : "image", // image, map, video
+				title       : "Nex", // image title
+				description : "Speed Optimized", // image description
+				link        : "images/1.jpg", // image src
+				thumb       : "images/1.jpg", // image thumb
+				url         : "#", // url where image will link
+				alt         : "Speed Optimized" // image alt tag
+			},
+			{
+				display     : "image", // image, map, video
+				title       : "Nex", // image title
+				description : "Speed Optimized", // image description
+				link        : "images/2.jpg", // image src
+				thumb       : "images/2.jpg", // image thumb
+				url         : "#", // url where image will link
+				alt         : "Speed Optimized" // image alt tag
+			},
+			{
+				display     : "image", // image, map, video
+				title       : "Nex", // image title
+				description : "Speed Optimized", // image description
+				link        : "images/3.jpg", // image src
+				thumb       : "images/3.jpg", // image thumb
+				url         : "#", // url where image will link
+				alt         : "Speed Optimized" // image alt tag
+			},
+			{
+				display     : "image", // image, map, video
+				title       : "Nex", // image title
+				description : "Speed Optimized", // image description
+				link        : "images/4.jpg", // image src
+				thumb       : "images/4.jpg", // image thumb
+				url         : "#", // url where image will link
+				alt         : "Speed Optimized" // image alt tag
+			},
+			{
+				display     : "image", // image, map, video
+				title       : "Nex", // image title
+				description : "Speed Optimized", // image description
+				link        : "images/5.jpg", // image src
+				thumb       : "images/5.jpg", // image thumb
+				url         : "#", // url where image will link
+				alt         : "Speed Optimized" // image alt tag
+			},
+			{
+				display     : "image", // image, map, video
+				title       : "Nex", // image title
+				description : "Speed Optimized", // image description
+				link        : "images/6.jpg", // image src
+				thumb       : "images/6.jpg", // image thumb
+				url         : "#", // url where image will link
+				alt         : "Speed Optimized" // image alt tag
+			},
+			{
+				display     : "image", // image, map, video
+				title       : "Nex", // image title
+				description : "Speed Optimized", // image description
+				link        : "images/7.jpg", // image src
+				thumb       : "images/7.jpg", // image thumb
+				url         : "#", // url where image will link
+				alt         : "Speed Optimized" // image alt tag
+			},
+			{
+				display     : "image", // image, map, video
+				title       : "Nex", // image title
+				description : "Speed Optimized", // image description
+				link        : "images/8.jpg", // image src
+				thumb       : "images/8.jpg", // image thumb
+				url         : "#", // url where image will link
+				alt         : "Speed Optimized" // image alt tag
+			},
+			{
+				display     : "image", // image, map, video
+				title       : "Nex", // image title
+				description : "Speed Optimized", // image description
+				link        : "images/9.jpg", // image src
+				thumb       : "images/9.jpg", // image thumb
+				url         : "#", // url where image will link
+				alt         : "Speed Optimized" // image alt tag
+			},
+			
+		]
+	});
+
+});
