e7c0f273d4b7e623477094a27d6695b06cd67790
zhangwj0101 <zhangwj0101@gmail.com>
2015-11-16 18:33:09 +0800

    添加

--- WebRoot/hlj_V2/kejizhanl/js/functions.js
+++ WebRoot/hlj_V2/kejizhanl/js/functions.js
@@ -8,14 +8,15 @@ jQuery(function() {
 		load_bar         : true, // true || false
 
 		data             : [
+
 			{
 				display     : "image", // image, map, video
 				title       : "Nex", // image title
-				description : "Speed Optimized", // image description
-				link        : "images/17.jpg", // image src
-				thumb       : "images/17.jpg", // image thumb
+				description : "Blazing Fast Fullscreen Slider", // image description
+				link        : "images/2.jpg", // image src
+				thumb       : "images/2.jpg", // image thumb
 				url         : "#", // url where image will link
-				alt         : "Speed Optimized" // image alt tag
+				alt         : "Blazing Fast Fullscreen Slider" // image alt tag
 			},
 			{
 				display     : "image", // image, map, video
@@ -41,21 +42,19 @@ jQuery(function() {
 				description : "Speed Optimized", // image description
 				link        : "images/3.jpg", // image src
 				thumb       : "images/3.jpg", // image thumb
-				link        : "images/02.jpg", // image src
-				thumb       : "images/02.jpg", // image thumb
+				url         : "#", // url where image will link
+				alt         : "Speed Optimized" // image alt tag
+			},
+			{
+				display     : "image", // image, map, video
+				title       : "Nex", // image title
+				description : "Speed Optimized", // image description
+				link        : "images/17.jpg", // image src
+				thumb       : "images/17.jpg", // image thumb
 				url         : "#", // url where image will link
 				alt         : "Speed Optimized" // image alt tag
 			},
 			
-			{
-				display     : "image", // image, map, video
-				title       : "Nex", // image title
-				description : "Blazing Fast Fullscreen Slider", // image description
-				link        : "images/3.jpg", // image src
-				thumb       : "images/3.jpg", // image thumb
-				url         : "#", // url where image will link
-				alt         : "Blazing Fast Fullscreen Slider" // image alt tag
-			},
 
 		]
 	});
